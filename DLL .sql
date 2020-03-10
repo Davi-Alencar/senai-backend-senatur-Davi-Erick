c9fbbfc903c07d523e8b8afa1d91f1d0f28119b1
Erick Henry <erick_henrry_blv@hotmail.com>
2020-03-10 14:42:08 -0300

    1

---
+++ DLL .sql
@@ -0,0 +1,33 @@
+CREATE DATABASE Senatur_Tarde;
+GO
+
+USE Senatur_Tarde;
+GO
+
+CREATE TABLE Pacotes (
+IdPacote			INT PRIMARY KEY IDENTITY, 
+NomePacote			VARCHAR (255) NOT NULL UNIQUE, 
+Descricao			VARCHAR (255) NOT NULL UNIQUE,
+DataIda				DATE, 
+DataVolta			DATE, 
+Valor				MONEY, 
+NomeCidade			VARCHAR (255) NOT NULL UNIQUE,
+Situacao			VARCHAR (255) NOT NULL UNIQUE
+);
+GO
+
+CREATE TABLE TipoUsuarios(
+IdTipoUsuario		INT PRIMARY KEY IDENTITY,
+Titulo				VARCHAR (255) NOT NULL UNIQUE
+);
+GO
+
+CREATE TABLE Usuarios(
+IdTipoUsuario INT PRIMARY KEY IDENTITY,
+Nome VARCHAR (255) NOT NULL
+Email			VARCHAR (255) NOT NULL,
+Senha			VARCHAR (255) NOT NULL,
+IdTipoUsuario	INT FOREIGN KEY REFERENCES  TiposUsuarios (IdTipoUsuario)
+);
+GO
+
