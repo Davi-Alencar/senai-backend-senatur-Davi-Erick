7b61d63daa94598b9b9c06b099ae25c03df69a04
Erick Henry <erick_henrry_blv@hotmail.com>
2020-03-10 17:28:25 -0300

    69

---
+++ Senai.Senatur.WebApi/Senai.Senatur.WebApi/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Senatur.WebApi.Interfaces
+{
+    interface IUsuarioRepository
+    {
+        /// <summary>
+        /// Cadastra um novo usuário
+        /// </summary>
+        /// <param name="novoUsuario">Objeto novoUsuario que será cadastrado</param>
+        void Cadastrar(Usuarios novoUsuario);
+
+        /// <summary>
+        /// Atualiza um usuário existente
+        /// </summary>
+        /// <param name="id">ID do usuário que será atualizado</param>
+        /// <param name="usuarioAtualizado">Objeto com as novas informações</param>
+        void Atualizar(int id, Usuarios usuarioAtualizado);
+
+        /// <summary>
+        /// Lista todos os usuários
+        /// </summary>
+        /// <returns>Uma lista de usuários</returns>
+        List<Usuarios> Listar();
+
+    }
+}
