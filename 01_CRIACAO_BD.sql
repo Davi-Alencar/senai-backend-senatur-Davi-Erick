384270ea4222190d6455bea2dc89e229dbd33019
Davi Alencar <davialencar101@gmail.com>
2020-03-10 16:04:44 -0300

    10/03/2020

--- 01_CRIACAO_BD.sql
+++ 01_CRIACAO_BD.sql
@@ -1,26 +1,23 @@
 CREATE DATABASE Senatur_Tarde;
 GO
 
-USE Senatur_Tarde;
-GO
+USE Senatur_Tarde
 
 CREATE TABLE Pacotes (
 IdPacote			INT PRIMARY KEY IDENTITY, 
-NomePacote			VARCHAR (255) NOT NULL UNIQUE, 
-Descricao			VARCHAR (255) NOT NULL UNIQUE,
+NomePacote			VARCHAR (255) NOT NULL, 
+Descricao			TEXT,
 DataIda				DATE, 
 DataVolta			DATE, 
 Valor				MONEY, 
-NomeCidade			VARCHAR (255) NOT NULL UNIQUE,
-Situacao			VARCHAR (255) NOT NULL UNIQUE
+NomeCidade			VARCHAR (255) NOT NULL,
+Ativo				BIT NOT NULL
 );
-GO
 
 CREATE TABLE TipoUsuarios(
 IdTipoUsuario		INT PRIMARY KEY IDENTITY,
 Titulo				VARCHAR (255) NOT NULL UNIQUE
 );
-GO
 
 CREATE TABLE Usuarios(
 IdUsuario INT PRIMARY KEY IDENTITY,
@@ -29,5 +26,3 @@ Email			VARCHAR (255) NOT NULL,
 Senha			VARCHAR (255) NOT NULL,
 IdTipoUsuario	INT FOREIGN KEY REFERENCES  TipoUsuarios (IdTipoUsuario)
 );
-GO
-
--- 02_INSERCAO_BD.sql
+++ 02_INSERCAO_BD.sql
@@ -1,13 +1,14 @@
 USE Senatur_Tarde;
 GO
 
-INSERT INTO Pacotes (NomePacote, Descricao, DataIda, DataVolta, Valor, NomeCidade, Situacao)
+
+INSERT INTO Pacotes (NomePacote, Descricao, DataIda, DataVolta, Valor, NomeCidade, Ativo)
 VALUES ('SALVADOR - 5 DIAS / 4 DIÁRIAS', 'O que não falta em Salvador são atrações. Prova disso são as praias,
 os museus e as construções seculares que dão um charme mais que especial à região. A
 cidade, sinônimo de alegria, também é conhecida pela efervescência cultural que a
 credenciou como um dos destinos mais procurados por turistas brasileiros e
 estrangeiros. O Pelourinho e o Elevador são alguns dos principais pontos de visitação.',
-'06/08/2020', '10/08/2020', '854.00', 'Salvador', 'Ativo'), 
+'06/08/2020', '10/08/2020', 854.00, 'Salvador', 1), 
 
 ('RESORTS NA BAHIA - LITORAL NORTE - 5 DIAS / 4 DIÁRIAS', 'O Litoral Norte da Bahia conta com
 inúmeras praias emolduradas por coqueiros, além de piscinas naturais de águas mornas
@@ -15,14 +16,14 @@ que são protegidas por recifes e habitadas por peixes coloridos. Banhos de mar e
 águas calmas ou agitadas, mergulho com snorkel, caminhada pela orla e calçadões,
 passeios de bicicleta, pontos turísticos históricos, interação com animais e até baladas
 estão entre as atrações da região. Destacam-se as praias de Guarajuba, Imbassaí,
-Praia do Forte e Costa do Sauipe.', '14/05/2020', '18/05/2020', '1826.00', 'Salvador', 'Ativo'),
+Praia do Forte e Costa do Sauipe.', '14/05/2020', '18/05/2020', 1826.00, 'Salvador', 1),
 
 ('BONITO VIA CAMPO GRANDE - 1 PASSEIO - 5 DIAS / 4 DIÁRIAS', 'Localizado no estado de Mato Grosso do
 Sul e ao sul do Pantanal, Bonito possui centenas de cachoeiras, rios e lagos de águas
 cristalinas, além de cavernas inundadas, paredões rochosos e uma infinidade de peixes.
 Os aventureiros costumam render-se facilmente a esse destino regado por trilhas
 ecológicas, passeios de bote e descidas de rapel pelas inúmeras quedas dágua da
-região.', '28/03/2020', '01/04/2020', '1004.00', 'Bonito', 'Ativo');
+região.', '28/03/2020', '01/04/2020', 1004.00, 'Bonito', 1);
 
 INSERT INTO TipoUsuarios (Titulo)
 VALUES ('Admin'), ('Comum');
@@ -30,5 +31,3 @@ VALUES ('Admin'), ('Comum');
 INSERT INTO Usuarios (Nome, Email, Senha, IdTipoUsuario)
 VALUES ('Ademir', 'admin@admin.com', 'admin', 1), 
 ('Cliente', 'cliente@cliente.com', 'cliente', 2);
-
-SELECT * FROM Pacotes
